9532d33059883cb812baae83d0557d7db2da3495
Till De Roquefeuil <tillderoquefeuil@MacBook-Pro-de-Till.local>
2020-04-30 17:52:12 +0200

    day01 ex06 test

--- day01/ex06/test.py
+++ day01/ex06/test.py
@@ -1,27 +1,23 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from my_linear_regression import MyLinearRegression as MyLR
-
 
 x = np.array([[12.4956442], [21.5007972], [31.5527382], [48.9145838], [57.5088733]])
 y = np.array([[37.4013816], [36.1473236], [45.7655287], [46.6793434], [59.5585554]])
 
-
 lr1 = MyLR([2, 0.7])
 
 # Example 0.0:
-print('\nExample 0.0')
-print(lr1.predict_(x))
-# Output: array([   [10.74695094],
-#                   [17.05055804], 
-#                   [24.08691674], 
-#                   [36.24020866], 
-#                   [42.25621131]])
+# print(lr1.predict_(x))
+# Output: 
+# array([   [10.74695094],
+#           [17.05055804], 
+#           [24.08691674], 
+#           [36.24020866], 
+#           [42.25621131]])
 
 # Example 0.1:
-print('\nExample 0.1')
-print(lr1.cost_elem_(lr1.predict_(x),y))
-
+# cost_elems = lr1.cost_elem_(lr1.predict_(x),y)
+# print(cost_elems)
 # Output:
 # array([   [77.72116511],
 #           [49.33699664], 
@@ -30,26 +26,21 @@ print(lr1.cost_elem_(lr1.predict_(x),y))
 #           [78.28360514]])
 
 # Example 0.2:
-print('\nExample 0.2')
-print(lr1.cost_(lr1.predict_(x),y))
-# print(mean_squared_error(lr1.predict_(x),y))
+# print(lr1.cost_(lr1.predict_(x),y))
 # Output:
 # 315.0202193084312
 
 
 lr2 = MyLR([0, 0])
-
 # Example 1.0:
-print('\nExample 1.0')
-lr2.fit_(x, y) 
+lr2.fit_(x, y, alpha=5e-8, n_cycle = 1500000)
 print(lr2.thetas)
-# Output: 
+# Output:
 # array([   [1.40709365],
-#           [1.1150909 ]])
+            # [1.1150909 ]])
 
 # Example 1.1:
-print('\nExample 1.1')
-print(lr2.predict_(x))
+# print(lr2.predict_(x))
 # Output: 
 # array([   [15.3408728 ],
 #           [25.38243697], 
@@ -58,8 +49,7 @@ print(lr2.predict_(x))
 #           [65.53471499]])
 
 # Example 1.2:
-print('\nExample 1.2')
-print(lr2.cost_elem_(lr2.predict_(x),y))
+# print(lr2.cost_elem_(lr1.predict_(x),y))
 # Output:
 # array([   [35.6749755 ],
 #           [ 4.14286023], 
@@ -68,9 +58,6 @@ print(lr2.cost_elem_(lr2.predict_(x),y))
 #           [22.27765992]])
 
 # Example 1.3:
-print('\nExample 1.3')
-print(lr2.cost_(lr2.predict_(x),y))
-# print(mean_squared_error(lr2.predict_(x),y))
+# print(lr2.cost_(lr1.predict_(x),y))
 # Output:
 # 92.66433192085971
-# Output:
